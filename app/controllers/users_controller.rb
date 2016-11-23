ca19e6ee6b28b1e649cd63f8a08dee98f2ded0ca
janenyasoro <janenyasoro@gmail.com>
2016-11-23 12:51:07 +0300

    confirming a user exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,29 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    #@user.email= 'your@gmail.com'
   end
 
   def show
+  	#@user = User.find(params[:id])
 
   end
+
   def create
+     @user = User.new(user_params)
+   if @user.save
+       redirect_to users_url
+    else
+      render 'new'
+    end
   end
 
-end
+private
+def user_params
+ params.require(:user).permit(:email, :password )
+   end
+
+
+   
+ end
+
